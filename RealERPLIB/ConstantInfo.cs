a981638efa166537b896eda015fd8461a77f3930
Parbaz-Hossain <md.parvezcse@gamil.com>
2021-08-21 14:44:33 +0600

    Second Changes

--- RealERPLIB/ConstantInfo.cs
+++ RealERPLIB/ConstantInfo.cs
@@ -25,7 +25,8 @@ namespace RealERPLIB
         /// <returns></returns>
         //nahid
         //Comment from dev branch 
-        //Comment from parvez brand 
+        //Comment from parvez branch 
+        //Second comment from parvez branch
 
         #region General Menu
         public static DataTable WebObjTable()
