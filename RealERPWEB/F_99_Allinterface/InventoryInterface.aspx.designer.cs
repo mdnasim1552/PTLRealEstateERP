c44faf9d8d046d07962294fe1faf932d9a6c3765
Tarikul <tarikul.asit@gmail.com>
2021-10-18 19:10:41 +0600

    inventory interface

--- RealERPWEB/F_99_Allinterface/InventoryInterface.aspx.designer.cs
+++ RealERPWEB/F_99_Allinterface/InventoryInterface.aspx.designer.cs
@@ -150,6 +150,24 @@ namespace RealERPWEB.F_99_Allinterface
         protected global::System.Web.UI.WebControls.GridView gvstatus;
 
         /// <summary>
+        /// pnlReqAprv control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlReqAprv;
+
+        /// <summary>
+        /// gvreqaprv control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvreqaprv;
+
+        /// <summary>
         /// pnlgatepass control.
         /// </summary>
         /// <remarks>
