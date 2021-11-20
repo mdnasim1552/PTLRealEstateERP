7c66273c2f7c72006a6ec65a3e987dd3c536889b
uzzalptl <uzzal08cse@gmail.com>
2021-11-20 10:06:10 +0600

    uzzal

--- RealERPWEB/F_81_Hrm/F_83_Att/HREmpAbsCtSpecial.aspx.cs
+++ RealERPWEB/F_81_Hrm/F_83_Att/HREmpAbsCtSpecial.aspx.cs
@@ -233,10 +233,7 @@ namespace RealERPWEB.F_81_Hrm.F_83_Att
         }
 
 
-        protected void gvabscount_RowDeleting(object sender, GridViewDeleteEventArgs e)
-        {
-
-        }
+       
 
 
         protected void imgbtnSearchEmployee_Click(object sender, EventArgs e)
@@ -244,27 +241,36 @@ namespace RealERPWEB.F_81_Hrm.F_83_Att
             this.ShowAbsCount();
 
         }
-        private void SaveValue()
-        {
+        //private void SaveValue()
+        //{
 
-            DataTable dt = (DataTable)Session["tblabscount"];
-            int rowindex;
+        //    DataTable dt = (DataTable)Session["tblabscount"];
+        //    int rowindex;
 
-            for (int i = 0; i < this.gvabsspecialcount.Rows.Count; i++)
-            {
+        //    for (int i = 0; i < this.gvabsspecialcount.Rows.Count; i++)
+        //    {
 
-                double absday = Convert.ToDouble("0" + ((TextBox)this.gvabsspecialcount.Rows[i].FindControl("txtabsday")).Text.Trim());
-                rowindex = (this.gvabsspecialcount.PageSize) * (this.gvabsspecialcount.PageIndex) + i;
-                dt.Rows[rowindex]["absday"] = absday;
-            }
+        //        double absday = Convert.ToDouble("0" + ((TextBox)this.gvabsspecialcount.Rows[i].FindControl("txtabsday")).Text.Trim());
+        //        rowindex = (this.gvabsspecialcount.PageSize) * (this.gvabsspecialcount.PageIndex) + i;
+        //        dt.Rows[rowindex]["absday"] = absday;
+        //    }
 
-            Session["tblabscount"] = dt;
-        }
+        //    Session["tblabscount"] = dt;
+        //}
 
 
         protected void lnkBtnEmployeeShow_Click(object sender, EventArgs e)
         {
-            this.getdata();
+
+            GridViewRow gvr = (GridViewRow)((LinkButton)sender).NamingContainer;
+            int RowIndex = gvr.RowIndex;
+            int rownumber = this.gvabsspecialcount.PageSize * this.gvabsspecialcount.PageIndex + RowIndex;
+            string empid = ((DataTable)Session["tblabscount"]).Rows[RowIndex]["empid"].ToString();
+            string empName = ((DataTable)Session["tblabscount"]).Rows[RowIndex]["empname"].ToString();
+            this.lblselectempid.Text = empid;
+            this.lblSelectEmpName.Text = "Employee Name : "+ empName;
+
+            this.GetEmployeeAbs(empid);          
             ScriptManager.RegisterStartupScript(this, GetType(), "alert", "openModal();", true);
 
             //ScriptManager.RegisterStartupScript(this, GetType(), "alert", "CloseModal_AlrtMsg();", true);
@@ -278,24 +284,13 @@ namespace RealERPWEB.F_81_Hrm.F_83_Att
             return (hst["comcod"].ToString());
         }
 
-        private void getdata()
+        private void GetEmployeeAbs(string empid)
         {
-            this.chkDate.Items.Clear();
-
-            //string comcod = this.GetCompCode();
-            ////string date = this.ddlyearmon.SelectedValue.ToString();
-
-            //string date = "01-Sep-2021";
-            ////this.ddlMonth.SelectedItem.Text.Substring(0, 3);
-            ////string year = ASTUtility.Right(this.ddlMonth.SelectedItem.Text.Trim(), 4);
-            ////string date = "01-" + Month + "-" + year;
-            //string empid = "930100101001";  //this.ddlEmpName.SelectedValue.ToString();
-
+            this.chkDate.Items.Clear();          
             string comcod = this.GetCompCode();
             string Month = this.ddlMonth.SelectedItem.Text.Substring(0, 3);
             string year = ASTUtility.Right(this.ddlMonth.SelectedItem.Text.Trim(), 4);
-            string date = "01-" + Month + "-" + year;
-            string empid = "";
+            string date = "01-" + Month + "-" + year;                                
             DataSet ds4 = HRData.GetTransInfo(comcod, "dbo_hrm.SP_ENTRY_EMPABSENT", "ABSENT_DATE", date, empid, "", "", "", "", "", "", "");
 
             if (ds4 == null)
@@ -327,12 +322,10 @@ namespace RealERPWEB.F_81_Hrm.F_83_Att
            this.Master.FindControl("lblmsg").Visible = true;
             Hashtable hst = (Hashtable)Session["tblLogin"];
             string comcod = hst["comcod"].ToString();
-            //string empid = this.ddlEmpName.SelectedValue.ToString();
+      
+           string empid = this.lblselectempid.Text.ToString();
 
-            GridViewRow row = (GridViewRow)((LinkButton)sender).NamingContainer;
-            int index = row.RowIndex;
-
-            string empid = ((LinkButton)this.gvabsspecialcount.Rows[index].FindControl("empid")).Text.ToString();
+            
             string month = this.ddlMonth.SelectedValue.ToString().Trim();
             string month1 = month.PadLeft(2, '0');
             string year = ASTUtility.Right(this.ddlMonth.SelectedItem.Text.Trim(), 4);
@@ -361,45 +354,11 @@ namespace RealERPWEB.F_81_Hrm.F_83_Att
             ((Label)this.Master.FindControl("lblmsg")).Text = "Updated Successfully";
 
         }
-        //protected void lbntUpdateAbs_Click(object sender, EventArgs e)
-        //{
-        //    this.Master.FindControl("lblmsg").Visible = true;
-        //    this.SaveValue();
-        //    DataTable dt = (DataTable)Session["tblabscount"];
-
-        //    //log Entry
-
-        //    Hashtable hst = (Hashtable)Session["tblLogin"];
-        //    string comcod = hst["comcod"].ToString();
-        //    string userid = hst["usrid"].ToString();
-        //    string Terminal = hst["compname"].ToString();
-        //    string Sessionid = hst["session"].ToString();
-        //    string PostedByid = userid;
-        //    string Posttrmid = Terminal;
-        //    string Posteddat = System.DateTime.Now.ToString("dd-MMM-yyyy hh:mm:ss tt");
-        //    string Monthid = ""; // this.ddlyearmon.Text.Trim();
-        //    for (int i = 0; i < dt.Rows.Count; i++)
-        //    {
-        //        string empid = dt.Rows[i]["empid"].ToString();
-        //        double absday = Convert.ToDouble(dt.Rows[i]["absday"]);
-
-        //        if (absday > 0)
-        //        {
-        //            bool result = HRData.UpdateTransInfo(comcod, "dbo_hrm.SP_ENTRY_EMPLOYEE01", "INSERTORUPDATEABSAUTO", Monthid, empid, absday.ToString(), PostedByid, Posttrmid, Posteddat, "", "", "", "", "", "", "", "", "");
-        //            if (!result)
-        //                return;
-
-        //        }
-        //    }
-        //    ((Label)this.Master.FindControl("lblmsg")).Text = "Updated Successfully";
-        //    ScriptManager.RegisterStartupScript(this, GetType(), "alert", "HideLabel(1);", true);
-
-        //}
-
+      
 
         protected void gvabscount_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
-            this.SaveValue();
+           
             this.gvabsspecialcount.PageIndex = e.NewPageIndex;
             this.Data_Bind();
 
@@ -407,7 +366,7 @@ namespace RealERPWEB.F_81_Hrm.F_83_Att
 
         protected void ddlpagesize_SelectedIndexChanged(object sender, EventArgs e)
         {
-            this.SaveValue();
+            
             this.Data_Bind();
 
 
