f9510e2af95460664dbdddfe89d325cb3a8363ab
emdadulhaque2000 <86713820+emdadulhaque2000@users.noreply.github.com>
2022-01-25 19:33:45 +0600

    emdad

--- RealERPWEB/F_17_Acc/AccPrint.aspx.cs
+++ RealERPWEB/F_17_Acc/AccPrint.aspx.cs
@@ -610,7 +610,7 @@ namespace RealERPWEB.F_17_Acc
                 case "3316"://  Assure(Development)
                 case "3317"://  Assure(Aggro)
                 case "3353":
-                case "3101"://  ASIT (Check)
+              //  case "3101"://  ASIT (Check)
 
                 case "3357"://  Cube Holding
 
@@ -733,7 +733,7 @@ namespace RealERPWEB.F_17_Acc
                     vouprint = "VocherPrintEntrust";
                     break;
 
-                case "3101":
+              
                 case "3364":
                     vouprint = "VocherPrintJBS";
                     break;
