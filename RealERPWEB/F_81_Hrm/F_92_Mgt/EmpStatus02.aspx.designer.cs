ceda1c14ec31bedc3ce06727e57017d16e008396
ibrahimptl <ibrahim.diu26@gmail.com>
2022-02-02 19:37:11 +0600

    khalil

--- RealERPWEB/F_81_Hrm/F_92_Mgt/EmpStatus02.aspx.designer.cs
+++ RealERPWEB/F_81_Hrm/F_92_Mgt/EmpStatus02.aspx.designer.cs
@@ -528,6 +528,15 @@ namespace RealERPWEB.F_81_Hrm.F_92_Mgt
         protected global::System.Web.UI.WebControls.View ViewPabx;
 
         /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
         /// inputtextbox control.
         /// </summary>
         /// <remarks>
