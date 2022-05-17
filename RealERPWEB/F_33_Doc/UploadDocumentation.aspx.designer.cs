24cc35bc0cc66039f2a4659348ef8fe80b3d4658
uzzalptl <uzzal08cse@gmail.com>
2022-05-17 18:32:51 +0600

    uzzal

--- RealERPWEB/F_33_Doc/UploadDocumentation.aspx.designer.cs
+++ RealERPWEB/F_33_Doc/UploadDocumentation.aspx.designer.cs
@@ -159,12 +159,12 @@ namespace RealERPWEB.F_33_Doc
         protected global::System.Web.UI.WebControls.LinkButton lnk_save;
 
         /// <summary>
-        /// gridService control.
+        /// gvdoc control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gridService;
+        protected global::System.Web.UI.WebControls.GridView gvdoc;
     }
 }
