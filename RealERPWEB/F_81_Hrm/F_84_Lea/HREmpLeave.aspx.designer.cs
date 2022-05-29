8cc90b68edaaa8a7ac4851c01277dc036d0fa41b
ibrahimptl <ibrahim.diu26@gmail.com>
2022-05-29 12:33:01 +0600

    khalil

--- RealERPWEB/F_81_Hrm/F_84_Lea/HREmpLeave.aspx.designer.cs
+++ RealERPWEB/F_81_Hrm/F_84_Lea/HREmpLeave.aspx.designer.cs
@@ -33,15 +33,6 @@ namespace RealERPWEB.F_81_Hrm.F_84_Lea
         protected global::System.Web.UI.UpdateProgress UpdateProgress2;
 
         /// <summary>
-        /// lnkReset control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnkReset;
-
-        /// <summary>
         /// LinkButton1 control.
         /// </summary>
         /// <remarks>
@@ -58,6 +49,15 @@ namespace RealERPWEB.F_81_Hrm.F_84_Lea
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton lnkRule;
+
+        /// <summary>
+        /// lnkReset control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lnkReset;
 
         /// <summary>
         /// divLeaveApp control.
