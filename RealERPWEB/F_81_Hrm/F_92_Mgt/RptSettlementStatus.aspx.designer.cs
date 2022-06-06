e212025c5c5db4934132e074c2bce6a2e1d59278
Parbaz-Hossain <md.parvezcse@gamil.com>
2022-06-06 19:25:41 +0600

    Employee Final Settlement

--- RealERPWEB/F_81_Hrm/F_92_Mgt/RptSettlementStatus.aspx.designer.cs
+++ RealERPWEB/F_81_Hrm/F_92_Mgt/RptSettlementStatus.aspx.designer.cs
@@ -42,13 +42,13 @@ namespace RealERPWEB.F_81_Hrm.F_92_Mgt
         protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
-        /// ddlWstation control.
+        /// ddlCompany control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlWstation;
+        protected global::System.Web.UI.WebControls.DropDownList ddlCompany;
 
         /// <summary>
         /// Label7 control.
