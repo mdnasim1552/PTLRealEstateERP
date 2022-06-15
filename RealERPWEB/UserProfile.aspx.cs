2b6edcf85d5d22f2f0f22e064d2b7f82a2390c20
uzzalptl <uzzal08cse@gmail.com>
2022-06-15 16:38:36 +0600

    uzzal

--- RealERPWEB/UserProfile.aspx.cs
+++ RealERPWEB/UserProfile.aspx.cs
@@ -321,19 +321,19 @@ namespace RealERPWEB
             if (ds1 == null)
                 return;
             DataTable dt = ds1.Tables[3];
-           
+
             string hrplc = "";
 
             for (int j = 0; j < dt.Rows.Count; j++)
             {
-                hrplc +=  "<div class='panel panel-default'>"+
-                                                "<div class='panel-heading'>"+
-                                                    "<h4 class='panel-title'>"+
-                                                        "<a data-toggle='collapse' data-parent='#accordionTwoLeft' href='#collapseTwoLeftone_"+j+"' aria-expanded='false' class='collapsed'>" + dt.Rows[j]["title"].ToString() +"</a>"+                                                       
+                hrplc += "<div class='panel panel-default'>" +
+                                                "<div class='panel-heading'>" +
+                                                    "<h4 class='panel-title'>" +
+                                                        "<a data-toggle='collapse' data-parent='#accordionTwoLeft' href='#collapseTwoLeftone_" + j + "' aria-expanded='false' class='collapsed'>" + dt.Rows[j]["title"].ToString() + "</a>" +
                                                     "</h4>" +
                                                 "</div>" +
-                                                "<div id='collapseTwoLeftone_"+j+"' class='panel-collapse collapse' aria-expanded='false' role='tablist' style='height: 0px;'>" +
-                                                    "<div class='panel-body'>" + dt.Rows[j]["details"].ToString() +"</div>" +
+                                                "<div id='collapseTwoLeftone_" + j + "' class='panel-collapse collapse' aria-expanded='false' role='tablist' style='height: 0px;'>" +
+                                                    "<div class='panel-body'>" + dt.Rows[j]["details"].ToString() + "</div>" +
                             "</div> </div>";
 
             }
