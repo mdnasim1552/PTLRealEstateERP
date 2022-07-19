42e38225c4c1523bf9da5973da9b6e3b5ca82f59
JulkarNime07 <81905049+JulkarNime07@users.noreply.github.com>
2022-07-19 19:33:34 +0600

    rakib

--- RealERPWEB/F_81_Hrm/F_85_Lon/InterfaceLoan.aspx.designer.cs
+++ RealERPWEB/F_81_Hrm/F_85_Lon/InterfaceLoan.aspx.designer.cs
@@ -735,6 +735,15 @@ namespace RealERPWEB.F_81_Hrm.F_85_Lon
         protected global::System.Web.UI.WebControls.LinkButton lnkCancel;
 
         /// <summary>
+        /// PrintLoan control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton PrintLoan;
+
+        /// <summary>
         /// lnkAdd control.
         /// </summary>
         /// <remarks>
