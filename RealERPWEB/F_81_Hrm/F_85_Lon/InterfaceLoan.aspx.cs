d5afb0e67f5c28c5b5593afc124d77db63a813a8
MD AHASAN ULLAH NAHID <ahasan2021@gmail.com>
2022-07-19 19:13:25 +0600

    20220717

--- RealERPWEB/F_81_Hrm/F_85_Lon/InterfaceLoan.aspx.cs
+++ RealERPWEB/F_81_Hrm/F_85_Lon/InterfaceLoan.aspx.cs
@@ -87,9 +87,7 @@ namespace RealERPWEB.F_81_Hrm.F_85_Lon
             DataTable dt = new DataTable();
             DataView dv = new DataView();
 
-
-
-
+             
 
             //pending
             dt = ((DataTable)ds1.Tables[1]).Copy();
