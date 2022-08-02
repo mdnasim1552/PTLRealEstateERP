271bc7492c133f0eeb73a4fda6ce7336cabd494e
tarikul93 <tarikul.asit@gmail.com>
2022-08-02 15:28:25 +0600

    tarik

--- RealERPWEB/F_22_Sal/RptSalSummery.aspx.designer.cs
+++ RealERPWEB/F_22_Sal/RptSalSummery.aspx.designer.cs
@@ -238,5 +238,23 @@ namespace RealERPWEB.F_22_Sal
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvCollvsHonoured;
+
+        /// <summary>
+        /// mSalesTarget control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View mSalesTarget;
+
+        /// <summary>
+        /// gvmSalesTarget control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvmSalesTarget;
     }
 }
