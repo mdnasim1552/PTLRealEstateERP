d1cfdbe383b4d52b2e13418d5843e064db76ed01
uzzalptl <uzzal08cse@gmail.com>
2022-09-07 17:47:59 +0600

    uzzal

--- RealERPWEB/F_17_Acc/AccPayment.aspx.designer.cs
+++ RealERPWEB/F_17_Acc/AccPayment.aspx.designer.cs
@@ -168,6 +168,15 @@ namespace RealERPWEB.F_17_Acc
         protected global::System.Web.UI.WebControls.LinkButton lnkOk;
 
         /// <summary>
+        /// chkgcactcode control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkgcactcode;
+
+        /// <summary>
         /// lblmsg control.
         /// </summary>
         /// <remarks>
