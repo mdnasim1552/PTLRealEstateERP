0bd2cac878b3d86ff9c34a466681c4661329f35c
JulkarNime07 <rakibinfo832@gmail.com>
2022-10-15 12:20:32 +0600

    rakib

--- RealERPWEB/F_81_Hrm/F_89_Pay/RpHRtPayroll.aspx.cs
+++ RealERPWEB/F_81_Hrm/F_89_Pay/RpHRtPayroll.aspx.cs
@@ -163,8 +163,12 @@ namespace RealERPWEB.F_81_Hrm.F_89_Pay
                         //    break;
                         default:
                             //string comcod = this.GetComCode();
+                            //this.txtfromdate.Text = System.DateTime.Today.AddMonths(-1).ToString("dd-MMM-yyyy");
+                            //this.txtfromdate.Text = "26" + this.txtfromdate.Text.Trim().Substring(2);
+                            //this.txttodate.Text = Convert.ToDateTime(this.txtfromdate.Text).AddMonths(1).AddDays(-1).ToString("dd-MMM-yyyy");
+
                             this.txtfromdate.Text = System.DateTime.Today.AddMonths(-1).ToString("dd-MMM-yyyy");
-                            this.txtfromdate.Text = "26" + this.txtfromdate.Text.Trim().Substring(2);
+                            this.txtfromdate.Text = "01" + this.txtfromdate.Text.Trim().Substring(2);
                             this.txttodate.Text = Convert.ToDateTime(this.txtfromdate.Text).AddMonths(1).AddDays(-1).ToString("dd-MMM-yyyy");
                             break;
                     }
