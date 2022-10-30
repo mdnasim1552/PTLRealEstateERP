11e8fd6fbaf46ee76a6c03e98709fea31f90603d
robixx <shorifulislam776@gmail.com>
2022-10-30 18:32:11 +0600

    shorif

--- RealEntity/C_38_AI/AIallPrint.cs
+++ RealEntity/C_38_AI/AIallPrint.cs
@@ -13,13 +13,13 @@ namespace RealEntity.C_38_AI
 
         public class InvoicePrint
         {
-             public string batchid { get; set; }
+            
              public string sircode { get; set; }
              public string prjid { get; set; }
              public string invno { get; set; }
              public string customer { get; set; }
              public string projectname { get; set; }
-             public string batchname { get; set; }
+            
              public string dataset { get; set; }
              public string nationality { get; set; }
              public string city { get; set; }
@@ -27,10 +27,11 @@ namespace RealEntity.C_38_AI
             public string notes { get; set; }
 
             public double qty { get; set; }
-             public DateTime invoicedate { get; set; }
+            public double totalrate { get; set; }
+            public DateTime invoicedate { get; set; }
              public DateTime duedate { get; set; }
              public string subjects { get; set; }
-             public double totalrate { get; set; }
+             
              public double totalamount { get; set; }
 
         }
