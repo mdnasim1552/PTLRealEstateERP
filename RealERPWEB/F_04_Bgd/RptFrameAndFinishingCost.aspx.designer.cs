c2409c896a572347c09f1477f23b08f1b7bdfdfe
Nazmulnayem <mdnazmulhasancse072@gmail.com>
2022-11-07 17:59:12 +0600

    nazmul

--- RealERPWEB/F_04_Bgd/RptFrameAndFinishingCost.aspx.designer.cs
+++ RealERPWEB/F_04_Bgd/RptFrameAndFinishingCost.aspx.designer.cs
@@ -69,6 +69,15 @@ namespace RealERPWEB.F_04_Bgd
         protected global::System.Web.UI.WebControls.LinkButton imgbtnFindProject;
 
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
         /// ddlProjectName control.
         /// </summary>
         /// <remarks>
