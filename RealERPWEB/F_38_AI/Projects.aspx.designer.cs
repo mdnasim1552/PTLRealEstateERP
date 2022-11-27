198456ac8ec6311f9be6055f27f959b8087d63aa
robixx <shorifulislam776@gmail.com>
2022-11-27 18:13:39 +0600

    shorif

--- RealERPWEB/F_38_AI/Projects.aspx.designer.cs
+++ RealERPWEB/F_38_AI/Projects.aspx.designer.cs
@@ -564,6 +564,15 @@ namespace RealERPWEB.F_38_AI
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
+        /// perrate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl perrate;
+
+        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
--- RealERPWEB/F_99_Allinterface/AIinterface.aspx.designer.cs
+++ RealERPWEB/F_99_Allinterface/AIinterface.aspx.designer.cs
@@ -1104,6 +1104,15 @@ namespace RealERPWEB.F_99_Allinterface
         protected global::System.Web.UI.WebControls.Label Label30;
 
         /// <summary>
+        /// perrate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl perrate;
+
+        /// <summary>
         /// Label29 control.
         /// </summary>
         /// <remarks>
