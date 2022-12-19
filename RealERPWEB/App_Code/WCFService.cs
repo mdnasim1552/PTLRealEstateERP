0ea9f50a553db45d08f198bc83e5c27a90da2f39
robixx <shorifulislam776@gmail.com>
2022-12-19 10:30:14 +0600

    shorif

--- RealERPWEB/App_Code/WCFService.cs
+++ RealERPWEB/App_Code/WCFService.cs
@@ -7,8 +7,9 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 using System.Data;
-
 using RealEntity;
+
+//using RealEntity;
 
 
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFService" in code, svc and config file together.
