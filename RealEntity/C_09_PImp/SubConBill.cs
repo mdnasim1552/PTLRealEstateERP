b9b5d583e330e80151286d9e3fc6cea1537d3b58
Nazmulnayem <mdnazmulhasancse072@gmail.com>
2022-12-27 17:38:12 +0600

    nazmul

--- RealEntity/C_09_PImp/SubConBill.cs
+++ RealEntity/C_09_PImp/SubConBill.cs
@@ -538,7 +538,7 @@ namespace RealEntity.C_09_PIMP
         }
 
 
-
+        [Serializable]
         public class EClassResourceWiseMBSum
         {
             public string rsircode { get; set; }
@@ -557,7 +557,29 @@ namespace RealEntity.C_09_PIMP
 
 
         }
+        [Serializable]
+        public class EClassMRbook
+        {
+            public string rsircode { get; set; }
+            public string rsirdesc { get; set; }
+            public string rsirunit { get; set; }
+            public string nos { get; set; }
+            public double lnght { get; set; }
+            public double breadth { get; set; }
+            public double height { get; set; }
+            public double uweight { get; set; }
+            public double tweight { get; set; }
+            public string remarks { get; set; }
 
+            public EClassMRbook()
+            {
+
+            }
+
+
+
+
+        }
 
     }
 
