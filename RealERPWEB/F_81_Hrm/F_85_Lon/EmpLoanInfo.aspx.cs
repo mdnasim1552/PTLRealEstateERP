40b4e87eca2686a87b2f4ae5003e5a7b6eac2bc9
rakibhosen <rakibinfo832@gmail.com>
2023-02-05 18:15:32 +0600

    rakib

--- RealERPWEB/F_81_Hrm/F_85_Lon/EmpLoanInfo.aspx.cs
+++ RealERPWEB/F_81_Hrm/F_85_Lon/EmpLoanInfo.aspx.cs
@@ -31,9 +31,9 @@ namespace RealERPWEB.F_81_Hrm.F_85_Lon
                         (DataSet)Session["tblusrlog"])) && !Convert.ToBoolean(hst["permission"]))
                     Response.Redirect("~/AcceessError.aspx");
 
-                DataRow[] dr1 = ASTUtility.PagePermission1(HttpContext.Current.Request.Url.AbsoluteUri.ToString(), (DataSet)Session["tblusrlog"]);
-                ((Label)this.Master.FindControl("lblTitle")).Text = dr1[0]["dscrption"].ToString();
-                this.Master.Page.Title = dr1[0]["dscrption"].ToString();
+                //DataRow[] dr1 = ASTUtility.PagePermission1(HttpContext.Current.Request.Url.AbsoluteUri.ToString(), (DataSet)Session["tblusrlog"]);
+                //((Label)this.Master.FindControl("lblTitle")).Text = dr1[0]["dscrption"].ToString();
+                //this.Master.Page.Title = dr1[0]["dscrption"].ToString();
 
                 //int indexofamp = (HttpContext.Current.Request.Url.AbsoluteUri.ToString().Contains("&")) ? HttpContext.Current.Request.Url.AbsoluteUri.ToString().IndexOf('&') : HttpContext.Current.Request.Url.AbsoluteUri.ToString().Length;
                 //if (!ASTUtility.PagePermission(HttpContext.Current.Request.Url.AbsoluteUri.ToString().Substring(0, indexofamp), (DataSet)Session["tblusrlog"]))
