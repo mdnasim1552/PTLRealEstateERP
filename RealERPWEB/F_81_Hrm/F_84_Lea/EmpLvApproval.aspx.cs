f70c8b9d26d19e9b28873d4cd9d47a82400a997a
rakibhosen <rakibinfo832@gmail.com>
2023-03-06 09:35:20 +0600

    rakib

--- RealERPWEB/F_81_Hrm/F_84_Lea/EmpLvApproval.aspx.cs
+++ RealERPWEB/F_81_Hrm/F_84_Lea/EmpLvApproval.aspx.cs
@@ -745,10 +745,10 @@ namespace RealERPWEB.F_81_Hrm.F_84_Lea
                     {
                         this.Chboxforward.Checked = true;
                     }
-                    else if (comcod == "3374" && roletype == "DPT")
-                    {
-                        this.Chboxforward.Checked = true;
-                    }
+                    //else if (comcod == "3374" && roletype == "DPT")
+                    //{
+                    //    this.Chboxforward.Checked = true;
+                    //}
                     string forword = Convert.ToBoolean(this.Chboxforward.Checked).ToString();
 
                     result = HRData.UpdateTransInfo(comcod, "dbo_hrm.SP_ENTRY_EMPLOYEE", "INSERTORUPEMLEAVAPP02", trnid, empid, gcod, frmdate, todate, applydat, forword, ishalfday, lbllevid, lapplied.ToString(), "", "", "", ApprovByid, Approvtrmid);
@@ -1419,10 +1419,11 @@ namespace RealERPWEB.F_81_Hrm.F_84_Lea
                 }else if (comcod == "3354" && roletype == "DPT")
                 {
                     this.Chboxforward.Checked = false;
-                }else if (comcod=="3374" && roletype== "DPT")
-                {
-                    this.Chboxforward.Checked = true;
                 }
+                //else if (comcod=="3374" && roletype== "DPT")
+                //{
+                //    this.Chboxforward.Checked = true;
+                //}
         
 
                 string isForward = Convert.ToBoolean(this.Chboxforward.Checked).ToString();
