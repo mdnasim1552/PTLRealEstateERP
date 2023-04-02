fc5329d737aeb2e79103685aba395190c03b0df3
developer6599 <developer9026599@gmail.com>
2023-04-02 13:57:49 +0600

    Ashik

--- RealERPWEB/F_21_MKT/RptCallCenterLead.aspx.designer.cs
+++ RealERPWEB/F_21_MKT/RptCallCenterLead.aspx.designer.cs
@@ -238,5 +238,23 @@ namespace RealERPWEB.F_21_MKT
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvConversionDetails;
+
+        /// <summary>
+        /// vRptConversion control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View vRptConversion;
+
+        /// <summary>
+        /// gvConversion control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvConversion;
     }
 }
