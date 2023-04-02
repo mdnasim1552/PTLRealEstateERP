8ff09dc6a77ba8ff26424195b41e8498ad56a208
EftekherAlam <dev1.pintech@gmail.com>
2023-04-02 09:14:40 +0600

    Eftkeher 02-04-2023 9:14 AM

--- RealERPWEB/F_99_Allinterface/PurchasePrint.aspx.cs
+++ RealERPWEB/F_99_Allinterface/PurchasePrint.aspx.cs
@@ -4559,6 +4559,7 @@ namespace RealERPWEB.F_99_Allinterface
 
                     //case "3101": //pintech                    
                     case "3354": //Edison Real Estate                        
+                        //Reportpath = "~/Report/RptPurchaseOrderEDRold.rdlc";
                         Reportpath = "~/Report/RptPurchaseOrderEDR.rdlc";
                         break;
 
