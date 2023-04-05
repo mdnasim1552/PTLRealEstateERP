a187fa7780681ff985f39a493d21f8e491fcc22e
Md. Nasim Uddin <developer9026592@gmail.com>
2023-04-05 16:37:44 +0600

    Add New page's data to grid view from sp and features of popup, print

--- RealERPWEB/F_23_CR/RptReconcilationLetter.aspx.cs
+++ RealERPWEB/F_23_CR/RptReconcilationLetter.aspx.cs
@@ -47,7 +47,7 @@ namespace RealERPWEB.F_23_CR
 
         private void Data_Bind()
         {
-            DataTable dt = (DataTable)Session["tblflrwisbill"];
+            DataTable dt = (DataTable)Session["RptReconcilationLetter"];
             this.gvflrwisbill.PageSize = Convert.ToInt32(this.ddlpagesize.SelectedValue.ToString());
             this.gvflrwisbill.DataSource = dt;
             this.gvflrwisbill.DataBind();
@@ -71,7 +71,7 @@ namespace RealERPWEB.F_23_CR
 
 
 
-            DataSet ds1 = purData.GetTransInfo(comcod, "SP_ENTRY_PURCHASE_02", "GETProjectsub", "", "", "", "", "", "", "", "", "");
+            DataSet ds1 = purData.GetTransInfo(comcod, "SP_ENTRY_PURCHASE_02", "GETALLSALESPROJECTS", "", "", "", "", "", "", "", "", "");
             if (ds1 == null)
                 return;
             this.ddlprjlist.DataTextField = "pactdesc";
@@ -84,15 +84,16 @@ namespace RealERPWEB.F_23_CR
         {
             string comcod = this.GetCompCode();
             string pactcode = this.ddlprjlist.SelectedValue.ToString();
-            string csircode = "98%";//this.ddlcontractorlist.SelectedValue.ToString() == "000000000000" ? "98%" : this.ddlcontractorlist.SelectedValue.ToString() + "%";
-            string billtcode = "%";//this.ddlcatagory.SelectedValue.ToString() == "000000000000" ? "%" : this.ddlcatagory.SelectedValue.ToString() + "%";
-            DataSet ds1 = purData.GetTransInfo(comcod, "SP_ENTRY_PURCHASE_02", "GETCONSTATEMENT", pactcode, csircode, billtcode, "", "", "", "", "", "");
+            string Desc2 = "asdas";
+            //string csircode = "98%";//this.ddlcontractorlist.SelectedValue.ToString() == "000000000000" ? "98%" : this.ddlcontractorlist.SelectedValue.ToString() + "%";
+            //string billtcode = "%";//this.ddlcatagory.SelectedValue.ToString() == "000000000000" ? "%" : this.ddlcatagory.SelectedValue.ToString() + "%";
+            DataSet ds1 = purData.GetTransInfo(comcod, "SP_ENTRY_PURCHASE_02", "GETCUSTOMERDETAILS", pactcode, "", "", "", "", "", "", "", "");
             if (ds1 == null)
             {
 
                 return;
             }
-            Session["tblflrwisbill"] = ds1.Tables[0];
+            Session["RptReconcilationLetter"] = ds1.Tables[0];
             this.Data_Bind();
 
         }
@@ -100,6 +101,54 @@ namespace RealERPWEB.F_23_CR
         protected void ddlprjlist_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        protected void lbtnView_Click(object sender, EventArgs e)
+        {
+            DataTable dt = (DataTable)Session["RptReconcilationLetter"];
+            int rowIndex = ((GridViewRow)((LinkButton)sender).NamingContainer).RowIndex;
+            string empId = dt.Rows[rowIndex]["Name"].ToString();
+            ScriptManager.RegisterStartupScript(this, GetType(), "alert", "OpenDedModal();", true);
+        }
+
+        private void RptReconsilationLetter()
+        {
+            Hashtable hst = (Hashtable)Session["tblLogin"];
+            string comcod = hst["comcod"].ToString();
+            //string comnam = hst["comnam"].ToString();
+            //string comadd = hst["comadd1"].ToString();
+            string compname = hst["compname"].ToString();
+            string session = hst["session"].ToString();
+            //string username = hst["username"].ToString();
+            //string printdate = System.DateTime.Now.ToString("dd.MM.yyyy hh:mm:ss tt");
+            //string printFooter = "Printed from Computer Address :" + compname + " ,Session: " + session + " ,User: " + username + " ,Time: " + printdate;
+            string ComLogo = new Uri(Server.MapPath(@"~\Image\LOGO" + comcod + ".jpg")).AbsoluteUri;
+            DataTable dt1 = (DataTable)Session["RptReconcilationLetter"];//Session["tblflrwisbill"];
+            if (dt1 == null || dt1.Rows.Count == 0)
+                return;
+            //var lst = dt1.DataTableToList<RealEntity.C_09_PIMP.SubConBill.RptPrjFloorWiseBill>();
+            LocalReport Rpt1 = new LocalReport();
+            Rpt1 = RealERPRDLC.RptSetupClass1.GetLocalReport("R_23_CR.RptReconcilationLetter", null, null, null);
+            Rpt1.EnableExternalImages = true;
+
+            //string selectedProjectText = ddlprjlist.SelectedItem.Text;
+            //Rpt1.SetParameters(new ReportParameter("comadd", comadd));
+            //Rpt1.SetParameters(new ReportParameter("selectedProjectText", selectedProjectText));
+            //Rpt1.SetParameters(new ReportParameter("comname", comnam));
+            Rpt1.SetParameters(new ReportParameter("ComLogo", ComLogo));
+            //Rpt1.SetParameters(new ReportParameter("printFooter", printFooter));
+
+            Session["Report1"] = Rpt1;
+
+            string url = "../RDLCViewer.aspx?PrintOpt=PDF";
+            string script = "window.open('" + url + "', '_blank');";
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "OpenWindow", script, true);
+
+        }
+
+        protected void lbtnPrintMail_Click(object sender, EventArgs e)
+        {
+            this.RptReconsilationLetter();
         }
     }
 }
