b4fbf231a63833b986c0c0a0f24724fff18e9e98
Md. Nasim Uddin <developer9026592@gmail.com>
2023-04-06 16:27:25 +0600

    Add New page's modification__

--- RealERPWEB/F_23_CR/RptReconcilationLetter.aspx.designer.cs
+++ RealERPWEB/F_23_CR/RptReconcilationLetter.aspx.designer.cs
@@ -130,5 +130,14 @@ namespace RealERPWEB.F_23_CR
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label unitLabel;
+
+        /// <summary>
+        /// projectLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label projectLabel;
     }
 }
