dfdfa6f4da84f857c4e694b3badd6cbd325bf681
rakibhosen <rakibinfo832@gmail.com>
2023-04-11 16:02:51 +0600

    rakib

--- RealERPWEB/F_81_Hrm/F_89_Pay/RpHRtPayroll.aspx.cs
+++ RealERPWEB/F_81_Hrm/F_89_Pay/RpHRtPayroll.aspx.cs
@@ -555,6 +555,9 @@ namespace RealERPWEB.F_81_Hrm.F_89_Pay
                 case "3376"://Angan Development
                     this.rbtlBonSheet.SelectedIndex = 17;
                     break;
+                case "3370"://CPDL
+                    this.rbtlBonSheet.SelectedIndex = 18;
+                    break;
 
                 default://Bridge  , Terranova        
                     this.rbtlBonSheet.SelectedIndex = 8;
@@ -1020,7 +1023,7 @@ namespace RealERPWEB.F_81_Hrm.F_89_Pay
             this.lblComBonLock.Text = (ds1.Tables[0].Rows.Count == 0) ? "False" : Convert.ToBoolean(ds1.Tables[0].Rows[0]["lock"]).ToString();
             string Calltype = (this.rbtlBonSheet.SelectedIndex == 0) ? "EMPBONUS" : (this.rbtlBonSheet.SelectedIndex == 1) ? "EMPBONUS1" : (this.rbtlBonSheet.SelectedIndex == 5) ? "EMPBONUS2" : (this.rbtlBonSheet.SelectedIndex == 2) ? "EMPBONUSSAN" : (this.rbtlBonSheet.SelectedIndex == 6) ? "EMPBONUSBRIDGE" : (this.rbtlBonSheet.SelectedIndex == 7) ? "EMPBONUSALLIANCE"
                 : (this.rbtlBonSheet.SelectedIndex == 8) ? "EMPBONUSGEN" : (this.rbtlBonSheet.SelectedIndex == 10) ? "EMPBONUSTROPICAL" : (this.rbtlBonSheet.SelectedIndex == 11) ? "EMPBONUSPEBSTEEL" : (this.rbtlBonSheet.SelectedIndex == 12) ? "EMPBONUSGREENWOOD" : (this.rbtlBonSheet.SelectedIndex == 13) ? "EMPBONUSBTI" : (this.rbtlBonSheet.SelectedIndex == 14) ? "EMPBONUSEDISON" 
-                : (this.rbtlBonSheet.SelectedIndex == 15) ? "EMPBONUSFINLAY" : (this.rbtlBonSheet.SelectedIndex == 16) ? "EMPBONUSLANCO": (this.rbtlBonSheet.SelectedIndex == 17) ? "EMPBONUSANGAN" : "EMPBONUSGEN";
+                : (this.rbtlBonSheet.SelectedIndex == 15) ? "EMPBONUSFINLAY" : (this.rbtlBonSheet.SelectedIndex == 16) ? "EMPBONUSLANCO": (this.rbtlBonSheet.SelectedIndex == 17) ? "EMPBONUSANGAN" : (this.rbtlBonSheet.SelectedIndex == 18) ? "EMPBONUSCPDL" : "EMPBONUSGEN";
             string afterdays = Convert.ToDouble("0" + this.txtafterdays.Text.Trim()).ToString();
             string comgross = this.Companygross();
             string bonpaytype = this.companyBonusPayType();
@@ -3926,6 +3929,10 @@ namespace RealERPWEB.F_81_Hrm.F_89_Pay
                         this.PrintBonusAngan();
                         break;
 
+                    case "3370"://Cpdl
+                        this.PrintBonusCPDL();
+                        break;
+
                     default:
                         this.PrintBonusSheet();
                         break;
@@ -3956,6 +3963,40 @@ namespace RealERPWEB.F_81_Hrm.F_89_Pay
             var lst = dt3.DataTableToList<RealEntity.C_81_Hrm.C_84_Lea.BO_ClassLeave.BonusSheet>();
             double tAmt = lst.Select(p => p.bonamt).Sum();
             Rpt1 = RealERPRDLC.RptSetupClass1.GetLocalReport("R_81_Hrm.R_89_Pay.RptBonusSheetAngan", lst, null, null);
+            Rpt1.EnableExternalImages = true;
+            Rpt1.SetParameters(new ReportParameter("comadd", comadd));
+            Rpt1.SetParameters(new ReportParameter("compname", comnam));
+            Rpt1.SetParameters(new ReportParameter("rptname", "Festival Bonus of " + bonusType + " for the Year' " + frmdate));
+            Rpt1.SetParameters(new ReportParameter("ComLogo", ComLogo));
+            Rpt1.SetParameters(new ReportParameter("printFooter", printFooter));
+            Rpt1.SetParameters(new ReportParameter("Inword", "In Word: " + ASTUtility.Trans(tAmt, 2)));
+
+            Session["Report1"] = Rpt1;
+            ((Label)this.Master.FindControl("lblprintstk")).Text = @"<script>window.open('../../RDLCViewer.aspx?PrintOpt=" +
+                        ((DropDownList)this.Master.FindControl("DDPrintOpt")).SelectedValue.Trim().ToString() + "', target='_blank');</script>";
+        }
+
+
+        private void PrintBonusCPDL()
+        {
+            Hashtable hst = (Hashtable)Session["tblLogin"];
+            string comcod = hst["comcod"].ToString();
+            string comnam = this.ddlCompany.SelectedItem.Text;
+            string compname = hst["compname"].ToString();
+            string comsnam = hst["comsnam"].ToString();
+            string comadd = hst["comadd1"].ToString();
+            string session = hst["session"].ToString();
+            string username = hst["username"].ToString();
+            string printdate = System.DateTime.Now.ToString("dd.MM.yyyy hh:mm:ss tt");
+            string printFooter = "Printed from Computer Address :" + compname + " ,Session: " + session + " ,User: " + username + " ,Time: " + printdate;
+            string ComLogo = new Uri(Server.MapPath(@"~\Image\LOGO" + comcod + ".jpg")).AbsoluteUri;
+            string bonusType = (this.chkBonustype.Checked) ? " EID-UL-ADHA" : "EID-UL-FITR";
+            string frmdate = Convert.ToDateTime(this.txtfromdate.Text).ToString("yyyy");
+            LocalReport Rpt1 = new LocalReport();
+            DataTable dt3 = (DataTable)Session["tblpay"];
+            var lst = dt3.DataTableToList<RealEntity.C_81_Hrm.C_84_Lea.BO_ClassLeave.BonusSheet>();
+            double tAmt = lst.Select(p => p.bonamt).Sum();
+            Rpt1 = RealERPRDLC.RptSetupClass1.GetLocalReport("R_81_Hrm.R_89_Pay.RptBonusSheetCPDL", lst, null, null);
             Rpt1.EnableExternalImages = true;
             Rpt1.SetParameters(new ReportParameter("comadd", comadd));
             Rpt1.SetParameters(new ReportParameter("compname", comnam));
@@ -4986,6 +5027,7 @@ namespace RealERPWEB.F_81_Hrm.F_89_Pay
                     }
                     break;
 
+                case "3370"://CPDL
                 case "3368": //Finlay 
                     i = 0;
                     foreach (GridViewRow gv1 in this.gvBonus.Rows)
