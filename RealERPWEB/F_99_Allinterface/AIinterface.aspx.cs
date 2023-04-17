725f9c6454ebc6020aa6f85f2151e713be964ad0
robixx <shorifulislam776@gmail.com>
2023-04-17 16:17:52 +0600

    shorif

--- RealERPWEB/F_99_Allinterface/AIinterface.aspx.cs
+++ RealERPWEB/F_99_Allinterface/AIinterface.aspx.cs
@@ -2732,9 +2732,13 @@ namespace RealERPWEB.F_99_Allinterface
             if (e.Row.RowType == DataControlRowType.DataRow)
             {
                 HyperLink hlink = (HyperLink)e.Row.FindControl("lnkbtnapproved");
+                HyperLink hlink1 = (HyperLink)e.Row.FindControl("lnkbtnmapiping");
                 string invono = Convert.ToString(DataBinder.Eval(e.Row.DataItem, "invno")).ToString().Trim();
+                string pactcode = Convert.ToString(DataBinder.Eval(e.Row.DataItem, "pactcode")).ToString().Trim();
                 string invoicedate = Convert.ToDateTime(DataBinder.Eval(e.Row.DataItem, "invoicedate")).ToString("dd-MMM-yyyy");
+                hlink1.Visible = pactcode == "" ? true : false;
                 hlink.NavigateUrl = "~/F_38_AI/AIInvoiceApproved.aspx?Type=Aproved&Invono=" + invono + "&Date=" + invoicedate;
+                hlink1.NavigateUrl = "~/F_34_Mgt/AccProjectCode.aspx";
 
             }
 
